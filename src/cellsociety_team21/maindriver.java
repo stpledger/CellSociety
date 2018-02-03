80cf8e487b8285282e4a551237f990c8605ed219
Harry Wang <hyw2@duke.edu>
2018-02-02 20:38:13 -0500

    reduced maindriver

--- src/cellsociety_team21/maindriver.java
+++ src/cellsociety_team21/maindriver.java
@@ -5,13 +5,9 @@ import java.awt.Desktop;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
 
 public class maindriver extends Application{
 	private static final String TITLE = "Cell Society Team 21";
-	private Scene scene;
-	private GridPane pane;
-	private Desktop desktop = Desktop.getDesktop();
 	
 	public static void main(String[] args) {
 		Application.launch(args);
